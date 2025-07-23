861d8470fa7c23319abce95752647fef6de35f76
Vineet1552 <99524765+Vineet1552@users.noreply.github.com>
2025-07-23 16:09:36 +0530

    first commit

---
+++ src/main/java/com/cdac/entities/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.cdac.entities;
+
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;	
+	
+	@CreationTimestamp
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
+	
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/cdac/entities/Vehicle.java
@@ -0,0 +1,42 @@
+package com.cdac.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "new_vehicles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Vehicle extends BaseEntity {
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	@NotNull(message = "User is required")
+	private UserEntity user;
+	
+	@NotBlank(message = "Model is required")
+	@Column(nullable = false)
+	private String model;
+	
+	@NotBlank(message = "Type is required")
+	@Column(nullable = false)
+	private String type;
+	
+	@NotBlank(message = "Registration number is required")
+    @Column(name = "registration_number", nullable = false, unique = true)
+	private String regNumber;
+	
+}
